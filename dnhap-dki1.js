33417114feff0cbdf4c55994a7c58becabd55a98
quocdat732 <quocdat732@gmail.com>
2024-03-01 15:31:25 +0700

    update all

--- dnhap-dki1.js
+++ dnhap-dki1.js
@@ -1,27 +1,27 @@
 const switchers = [...document.querySelectorAll('.switcher')]
 
 switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
+    item.addEventListener('click', function () {
+        switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+        this.parentElement.classList.add('is-active')
+    })
 })
 
-let acc = [
-    {
-        id: 1,
-        email: "admin@gmail.com",
-        username: "admin",
-        password: "admin",
-    },
-    {
-        id: 2,
-        email: "admin2@gmail.com",
-        username: "admin2",
-        password: "admin2",
-    }
-];
-localStorage.setItem("account", JSON.stringify(acc));
+// let acc = [
+//     {
+//         id: 1,
+//         email: "admin@gmail.com",
+//         username: "admin",
+//         password: "admin",
+//     },
+//     {
+//         id: 2,
+//         email: "admin2@gmail.com",
+//         username: "admin2",
+//         password: "admin2",
+//     }
+// ];
+// localStorage.setItem("account", JSON.stringify(acc));
 
 // function getValue () {
 //     event?.preventDefault();
@@ -57,22 +57,22 @@ localStorage.setItem("account", JSON.stringify(acc));
 let dataAcc = JSON.parse(localStorage.getItem("account"))
 console.log("🚀 ~ file: index.js:49 ~ dataAcc:", dataAcc)
 
-function checkEmail(email, password, type){
+function checkEmail(email, password, type) {
     console.log(checkEmail)
     let exists = false;
 
-    for(let i = 0; i < dataAcc.length; i++)
-        if(email === dataAcc[i].email){
-            if(type === 'login'){
+    for (let i = 0; i < dataAcc.length; i++)
+        if (email === dataAcc[i].email) {
+            if (type === 'login') {
                 //neu loai la login, kiem tra ca password
-                if (password === dataAcc[i].password){
+                if (password === dataAcc[i].password) {
                     exists = true;
                 }
             } else {
                 //neu loai khong phai 'login', chi can kiem tra email
                 exists = true;
             }
-        return exists
+            return exists
         }
 }
 
@@ -83,20 +83,41 @@ function handleRegister() {
     let valueConfPassword = document.getElementById('confirm-password').value;
     if (valueEmail !== '' && valuePassword !== '') {
         if (checkEmail(valueEmail)) {
-            alert('Email already exists')
+            Swal.fire({
+                icon: "error",
+                title: "Error!",
+                text: "Email already exists",
+            });
         } else {
             if (valuePassword === valueConfPassword) {
                 dataAcc.push(
                     { id: dataAcc.length + 1, email: valueEmail, password: valuePassword }
                 )
                 localStorage.setItem('account', JSON.stringify(dataAcc))
-                alert('Register success') // ngược lại thông báo email hợp lệ 
+                Swal.fire({
+                    position: "bottom-end",
+                    icon: "success",
+                    title: "Register success",
+                    showConfirmButton: false,
+                    timer: 1500
+                }); // thông báo đăng ký thành công
+                setTimeout(() => {
+                    window.location.href = 'dnhap-dki1.html'
+                }, 1500)// sau 1,5s sẽ chuyển sang trang chủ
             } else {
-                alert('Confirm Password fail') // ngược lại thông báo email hợp lệ
+                Swal.fire({
+                    icon: "error",
+                    title: "Error!",
+                    text: "Confirm Password fail",
+                });// xác nhận password sai
             }
         }
     } else {
-        alert('Please enter your email and password')
+        Swal.fire({
+            icon: "warning",
+            title: "Warning!",
+            text: "Please enter your email and password",
+        });
     }
 }
 
@@ -107,15 +128,31 @@ function handleLogin() {
     let valuePassword = document.getElementById('login-password').value;
     if (valueEmail !== '' && valuePassword !== '') {
         if (checkEmail(valueEmail, valuePassword.toString(), 'login')) {
-            Swal.fire("Login successful");
+            localStorage.setItem('isLogin', true) // lưu trạng thái đăng nhập là true
+            Swal.fire({
+                position: "bottom-end",
+                icon: "success",
+                title: "Login successful",
+                showConfirmButton: false,
+                timer: 1500
+            }); // thông báo đăng nhập thành công
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 1500)// sau 1,5s sẽ chuyển sang trang chủ
         } else {
             Swal.fire({
                 icon: "error",
                 title: "Oops...",
                 text: "Account not found",
-              });
+            });
         }
     } else {
-        alert('Please enter email and password')
+        Swal.fire({
+            icon: "warning",
+            title: "Warning!",
+            text: "Please enter email and password",
+        });
     }
 }
+
+
--- index.js
+++ index.js
@@ -1,199 +1,46 @@
-document.addEventListener("DOMContentLoaded",function(){
-	var nut = document.querySelectorAll("button");
-	var nut = nut[0];
-	var menu = document.querySelectorAll(".menu");
-	var menu = menu[0];
-	var manhinhden = document.querySelectorAll(".manhinhden");
-	var manhinhden = manhinhden[0];
-	nut.onclick = function(){
-		menu.classList.add('menuhienra');
-		manhinhden.classList.add('manhinhdenhienra');
-	}
-	manhinhden.onclick = function(){
-		manhinhden.classList.remove('manhinhdenhienra');
-		menu.classList.remove('menuhienra');
-	}
-},false)
-
-let data = [
-    {
-        id: 1,
-        namePro: "croptop",
-        image: "https://i.pinimg.com/originals/eb/a9/67/eba96785c5a65bc657e7b075fb689739.jpg",
-        dateCreated: "10/11/2023",
-        desc: "description",
-        price: "23$"
-    },
-    {
-        id: 2,
-        namePro: "Ghế",
-        // image: "https://i.pinimg.com/originals/6b/47/68/6b4768c2c2cdc6a0e12e67397631beff.jpg",
-        image: "https://i.pinimg.com/originals/9c/fd/3c/9cfd3c6157afe40bebdc6cc86f699c9a.jpg",
-        dateCreated: "03/11/2023",
-        desc: "description",
-        price: "1$"
-    },
-    {
-        id: 3,
-        namePro: "Đèn",
-        image: "https://i.pinimg.com/originals/83/c7/ff/83c7ff5d929a0d1c9a7faa517fcdc856.jpg",
-        dateCreated: "10/01/2023",
-        desc: "description",
-        price: "20$"
-    },
-    {
-        id: 4,
-        namePro: "Tủ",
-        image: "https://i.pinimg.com/originals/a8/43/b5/a843b5497b2988fc4ab218d5577a4c69.jpg",
-        dateCreated: "23/11/2023",
-        desc: "description",
-        price: "3$"
-    },
-    {
-        id: 5,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/ad/51/41/ad51419441e645ba306a8eb8c74893c2.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "9$"
-    },
-    {
-        id: 6,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/f4/07/b1/f407b1f0d48b7b06ce1da1ca1819fffb.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
+document.addEventListener("DOMContentLoaded", function () {
+    var nut = document.querySelectorAll("button");
+    var nut = nut[0];
+    var menu = document.querySelectorAll(".menu");
+    var menu = menu[0];
+    var manhinhden = document.querySelectorAll(".manhinhden");
+    var manhinhden = manhinhden[0];
+    nut.onclick = function () {
+        menu.classList.add('menuhienra');
+        manhinhden.classList.add('manhinhdenhienra');
     }
-    ,
-    {
-        id: 7,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/d5/2e/48/d52e489bb8385b376f93d6f82428fb43.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    },
-    {
-        id: 8,
-        namePro: "Ghế",
-        image: "https://i.pinimg.com/originals/62/30/87/623087de9864fefa7c0ebb11b543628c.jpg",
-        dateCreated: "03/11/2023",
-        desc: "description",
-        price: "1$"
-    },
-    {
-        id: 9,
-        namePro: "Đèn",
-        image: "https://i.pinimg.com/originals/14/54/41/145441e8f40bcef6190256d6fd548ed6.jpg",
-        dateCreated: "10/01/2023",
-        desc: "description",
-        price: "20$"
-    },
-    {
-        id: 10,
-        namePro: "Tủ",
-        image: "https://i.pinimg.com/originals/a8/43/b5/a843b5497b2988fc4ab218d5577a4c69.jpg",
-        dateCreated: "23/11/2023",
-        desc: "description",
-        price: "3$"
-    },
-    {
-        id: 11,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/ad/51/41/ad51419441e645ba306a8eb8c74893c2.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "9$"
-    },
-    {
-        id: 12,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/0e/35/45/0e354568022aedc793215c8f1da95314.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
+    manhinhden.onclick = function () {
+        manhinhden.classList.remove('manhinhdenhienra');
+        menu.classList.remove('menuhienra');
     }
-    ,
-    {
-        id: 13,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/9d/58/67/9d5867f8ac14fb9020c0678c465ab6b4.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    },
-    {
-        id: 14,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/ad/51/41/ad51419441e645ba306a8eb8c74893c2.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "9$"
-    },
-    {
-        id: 15,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/88/19/c0/8819c09f35a82640af644d0d0689c1f3.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    }
-    ,
-    {
-        id: 16,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/d5/2e/48/d52e489bb8385b376f93d6f82428fb43.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    },
-    {
-        id: 17,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/ad/51/41/ad51419441e645ba306a8eb8c74893c2.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "9$"
-    },
-    {
-        id: 18,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/f4/07/b1/f407b1f0d48b7b06ce1da1ca1819fffb.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    }
-    ,
-    {
-        id: ,
-        namePro: "Laptop",
-        image: "https://i.pinimg.com/originals/d5/2e/48/d52e489bb8385b376f93d6f82428fb43.jpg",
-        dateCreated: "09/09/2023",
-        desc: "description",
-        price: "19$"
-    }
-];
-
+}, false)
 let htmlProduct = "";
 
-for (let i = 0; i < data.length; i++) {
-    htmlProduct +=
-    `
-    <div class="card">
-    <img class="img-pro" src="${data[i].image}"/>
-    <div class="card-body">
-            <h5 class="card-title">${data[i].namePro}</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-body-secondary">Price: ${data[i].price}</small></p>
-    </div>
-    </div>
+fetch('https://api.myjson.online/v1/records/66712e0a-04de-44bc-8f37-4dbd638c21f0')
+    .then((res) => res.json())
+    .then((response) => {
+        console.log("🤔🤔🤔 ~ response:", response.data)
+        for (let i = 0; i < response.data.length; i++) {
+            htmlProduct +=
+                `
+            <div class="card">
+            <img class="img-pro" src="${response.data[i].image}"/>
+            <div class="card-body">
+                    <h5 class="card-title">${response.data[i].namePro}</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text price">Price: ${response.data[i].price}</p>
+                <button class="button-28" role="button">Detail</button>    
+            </div>
+            </div>
+            `
+        }
 
-   
-    `
-}
+        document.getElementById("card-group").innerHTML = htmlProduct;
+    })
 
-document.getElementById("card-group").innerHTML = htmlProduct;
+
+
+
 
 // function filterproduct(value){
 //     let buttons = document.querySelectorAll(".selections");
@@ -224,3 +71,17 @@ document.getElementById("card-group").innerHTML = htmlProduct;
 //     //Bắt đầu chạy thôi :D
 //     moveRight();
 // });
+
+
+
+//kiểm tra nếu isLogin dưới localStorage là true thì ẩn nút đăng nhập đăng ký và hiện nút logout
+if (localStorage.getItem('isLogin') == 'true') {
+    document.getElementById('login-register').classList.add('hidden-menu')// ẩn menu đăng nhập đăng ký
+    document.getElementById('show-logout').classList.remove('btn-logout')// hiện nút đăng xuất
+}
+
+
+function handleLogout() {
+    localStorage.setItem('isLogin', false) // lưu trang thái đăng nhập isLogin dưới localStorage lại thành false nghĩa là chưa đăng nhập
+    window.location.reload() // load lại trang
+}
